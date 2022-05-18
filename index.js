fa75f853e682f39addb71fdb7a732f5063abbaac
ojimcy <ojimcy247@gmail.com>
2022-05-18 17:31:11 +0100

    template home page

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+const {engine} = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+//Handlebars
+app.engine('.hbs', engine({defaltLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, console.log(`Server started on ${PORT}`))
