f57d830424c5d1a8ea75454c1cfa58b09c0ec9ac
ojimcy <ojimcy247@gmail.com>
2022-05-31 18:35:21 +0100

    form thank you page

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const path = require('path')
 const {engine} = require('express-handlebars')
-const bodyParser = require('body-parser')
-
-const nodemailer = require('nodemailer')
-const { text } = require('stream/consumers')
 
 const app = express()
 
@@ -31,33 +27,8 @@ app.get('/private-haven', (req, res) => {
     res.render('private-haven');
 })
 
-app.post('/', (req, res) => {
-    // console.log(req.body);
-
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'ojayconnect@gmail.com',
-            password: 'ojima123'
-        }
-    })
-
-    const mailOptions = {
-        from: req.body.email,
-        to: 'ojayconnect@gmail.com',
-        subject: `Message from ${req.body.email}: ${req.body.subject}`,
-        text: req.body.message
-    }
-
-    transporter.sendMail(mailOptions, (error, info)=>{
-        if (error) {
-            console.log('error');
-            res.send('error')
-        }else {
-            console.log('Email sent: ' + info.response);
-            res.send('Success')
-        }
-    })
+app.get('/form-success', (req, res) => {
+    res.render('form-success');
 })
 
 const PORT = process.env.PORT || 3000
